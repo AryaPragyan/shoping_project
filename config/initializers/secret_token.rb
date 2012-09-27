67aa5d384fd71dce75ea42cef1023e1254cc848a
Arya <arya.pragyan@sumerusolutions.com>
2012-09-27 12:44:06 +0530

    Shoping commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShopingProject::Application.config.secret_token = '25b15322d269d956042b162b1fa120524059f24bb522a1d061d686b5707e1caaa9107dc71de97b12f2a31f4a7ee2bf43b130f65f49a868f5cc72b6bb66f7807c'
